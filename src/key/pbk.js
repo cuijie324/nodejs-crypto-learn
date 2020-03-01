ec70b303b71247dc5ef31786a84f2c2b8525ff8d
Cui Jie <yinsu@foxmail.com>
2020-03-01 20:55:52 +0800

    pbkdf2

--- src/key/pbk.js
+++ src/key/pbk.js
@@ -1,9 +1,10 @@
 const crypto = require('crypto');
 
+// 生成密钥
 // crypto.pbkdf2('secret', 'salt', 10000, 64, 'sha512', (err, derivedKey) => {
 //     if (err) throw err;
 //     console.log(derivedKey.toString('hex'));
 // });
 
-let key = crypto.pbkdf2Sync('secret', 'salt', 10000, 64, 'sha512');
+let key = crypto.pbkdf2Sync('secret', 'salt', 10000, 32, 'sha512');
 console.log(key.toString('hex'));
